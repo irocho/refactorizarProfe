b54ba148c941c02a3b4d7d458ef07d7aa7c0cb61
i-rochiño <irocho.profe@gmail.com>
2026-03-23 13:29:28 +0000

    px74

---
+++ px74.java
@@ -0,0 +1,38 @@
+// /**
+//  * Mostra a foto e logo imprime a identidade e idade.
+//  * @param identidade
+//  * @param idade
+//  * @see  SEN REFACTORIZAR
+//  */
+// void visualizarDatos(String identidade, int idade) {
+//     mostrarFoto();
+//     System.out.println("Identidade: " + identidade);
+//     System.out.println("Idade: " + idade);
+// }
+
+// Redución lóxica
+
+
+/**
+ * Mostrar a foto e logo imprime a identidade e idade.
+ * 
+ * @param ident
+ * @param idade
+ * @see REFACTORIZADO
+ */
+void visualizarDatosRef(String ident, int idade) {
+    mostrarFoto();
+    mostrarDatos(ident, idade);
+
+}
+/**
+ * Imprime a identidade e idade.
+ * 
+ * @param ident
+ * @param anos
+ * @see REFACTORIZADO
+ */
+void mostrarDatos(String ident, int anos) {
+    System.out.println("Identidade: " + ident);
+    System.out.println("Idade: " + anos);
+}
---
+++ px74_profe.java
@@ -0,0 +1,20 @@
+// Redución lóxica executable
+public class px74_profe {
+    public static void main(String[] args) {
+        System.out.println("Redución lóxica");
+    }
+}
+    void mostrarFoto() {
+
+    void visualizarDatosRef(String ident, int idade) {
+        mostrarFoto();
+        mostrarDatos(ident, idade);
+
+    }
+
+    void mostrarDatos(String ident, int anos) {
+        System.out.println("Identidade: " + ident);
+        System.out.println("Idade: " + anos);
+    }
+
+}
