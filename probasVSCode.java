5ef9311b2e8eee4b45bc9dcb02651d990415747b
i-rochiño <irocho.profe@gmail.com>
2026-04-16 08:44:08 +0000

    ficheiro refactorizado

---
+++ probasVSCode.java
@@ -0,0 +1,49 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+public class probasVSCode {
+
+    private static final double PI = 3.14;
+    private int area;
+
+    public static void main(String[] args) {
+        System.out.println("Casos de uso");
+
+//Caso 1: Poñer nome ás variables ======================================================
+        List<String> colorins = Arrays.asList("vermello", "laranxa", "verde");
+        System.err.println(colorins.get(2));
+
+//Caso 2: Mellora de lazos ou bucles ======================================================
+        String[] libros = {"Don Quijote", "Crimen e castigo"};
+        for (String libro : libros) {
+            System.out.println(libro);
+        }
+    }
+
+//Caso 3: crear constantes ======================================================
+    public double getArea(double r) {
+        return PI * r * r;
+    }
+
+// Caso 4: extraer a método ======================================================
+    public void extraerMetodo() {
+        int altura = 1;
+        int base = 2;
+        calculoArea(altura, base);
+    }
+
+    private int calculoArea(int altura, int base) {
+        return area = base * altura;
+    }
+
+//Caso 5: Extraer variable ======================================================
+    String ordenador = "MAC";
+    public void extraerVar() {
+        boolean seraMAC = ordenador.equalsIgnoreCase("MAC");
+        if (seraMAC) {
+            System.out.println("é un Mac");
+        } 
+    }
+
+}
